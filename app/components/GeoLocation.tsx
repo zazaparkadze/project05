c9ed489cc26044259f7de45c1087624e836a2deb
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-10-02 18:52:09 +0300

    coordenates

---
+++ app/components/GeoLocation.tsx
@@ -0,0 +1,63 @@
+"use client";
+import clsx from "clsx";
+import { useState } from "react";
+
+type WithTimeStamp = {
+  lat: string | undefined;
+  lgt: string | undefined;
+  timestamp: string | undefined;
+};
+
+export default function GeoLocation() {
+  const [notEmpty, setNotEmpty] = useState(false);
+  const initialValue: WithTimeStamp = {
+    lat: undefined,
+    lgt: undefined,
+    timestamp: undefined,
+  };
+
+  const [latitude_longitude, setLatitude_Longitude] = useState(initialValue);
+  function getLocation() {
+    function showPosition(position: GeolocationPosition) {
+      x.innerHTML =
+        "Latitude: " +
+        position.coords.latitude +
+        "<br>Longitude: " +
+        position.coords.longitude +
+        "<br> Timestamp: " +
+        new Date(position.timestamp);
+      setNotEmpty(true);
+      setLatitude_Longitude({
+        lat: position.coords.latitude + "",
+        lgt: position.coords.longitude + "",
+        timestamp: new Date(position.timestamp).toString(),
+      });
+    }
+
+    const x = document.getElementById("demo") as HTMLParagraphElement;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={() => getLocation()} className="text-4xl text-center">
+        Get Your Coordinates.
+      </button>
+      <p
+        id="demo"
+        className={clsx({
+          "text-4xl text-blue-900  py-3 px-5 bg-gradient-to-l from-amber-600 to-amber-300 font-semibold rounded-[20px]":
+            notEmpty,
+          "": !notEmpty,
+        })}
+      ></p>
+      <p>{latitude_longitude.lat}</p>
+      <p>{latitude_longitude.lgt}</p>
+      <p>{latitude_longitude.timestamp}</p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,45 @@
 import Link from "next/link";
 import ProfilePic from "./components/ProfilePic";
+import GeoLocation from "./components/GeoLocation";
 
 export default function Home() {
   return (
-    <div className=" group dark: text-white p-3 m-3 flex justify-center items-center gap-5 md:flex-col">
-      <ProfilePic />
-      <p className="text-3xl mt-10 mb-10 text-center">
-        Hello and Welcome, 👋 &nbsp;
-        <span className="whitespace-nowrap">I am Zaza</span>
-      </p>
-      <div
-        className="absolute
+    <div className="text-white">
+      <div className="group dark: text-white p-3 m-3 flex justify-center items-center gap-5 md:flex-col">
+        <ProfilePic />
+        <p className="text-3xl mt-10 mb-10 text-center">
+          Hello and Welcome, 👋 &nbsp;
+          <span className="whitespace-nowrap">I am Zaza</span>
+        </p>
+        <div
+          className="absolute
            text-2xl font-bold group-hover:opacity-100 group-hover:visible group-hover:text-white/100
            bg-transparent text-gold/50 rounded-md py-1 px-2 top-12 right-12
            opacity-0 transition-all duration-3000 z-50 group-hover:mx-auto"
-      >
-        <ul>
-          <li className="brightness-70 hover:brightness-150">
-            <Link href={"http://project01-gold.vercel.app"} target="_blank">
-              JsonPlaceholder
-            </Link>
-          </li>
-          <li className="brightness-70 hover:brightness-150">
-            <Link href={"http://project02-gold.vercel.app"} target="_blank">
-              WikiRocket
-            </Link>
-          </li>
-          <li className="brightness-70 hover:brightness-150">
-            <Link
-              href={"http://project03-gold-mocha.vercel.app"}
-              target="_blank"
-            >
-              Weather Api
-            </Link>
-          </li>
-        </ul>
+        >
+          <ul>
+            <li className="brightness-70 hover:brightness-150">
+              <Link href={"http://project01-gold.vercel.app"} target="_blank">
+                JsonPlaceholder
+              </Link>
+            </li>
+            <li className="brightness-70 hover:brightness-150">
+              <Link href={"http://project02-gold.vercel.app"} target="_blank">
+                WikiRocket
+              </Link>
+            </li>
+            <li className="brightness-70 hover:brightness-150">
+              <Link
+                href={"http://project03-gold-mocha.vercel.app"}
+                target="_blank"
+              >
+                Weather Api
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
+      <GeoLocation />
     </div>
   );
 }
-
-/* 
- <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-            Hover me
-          </button> */
